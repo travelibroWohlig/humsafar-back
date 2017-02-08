f1dde010cc88d30a442ae3fc3ce50b40d04453a4
sohan1 <shn619@gmail.com>
2017-02-08 13:25:06 +0530

    mustdo

--- api/services/Mustdocity.js
+++ api/services/Mustdocity.js
@@ -35,7 +35,7 @@ var schema = new Schema({
 schema.plugin(deepPopulate, {});
 schema.plugin(uniqueValidator);
 schema.plugin(timestamps);
-module.exports = mongoose.model('MustdoCity', schema);
+module.exports = mongoose.model('Mustdocity', schema);
 
 var exports = _.cloneDeep(require("sails-wohlig-service")(schema, 'mustdocity', 'mustdocity'));
 var model = {};
