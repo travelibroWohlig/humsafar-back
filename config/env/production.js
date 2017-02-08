96ed1d18b40d35eb5b1b40be932e2b718e7e42f8
Chintan Shah <chintan@wohlig.com>
2017-02-08 12:32:12 +0530

    port change

--- config/env/production.js
+++ config/env/production.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 80,
+  port: 83,
   realHost: "http://travelibrobackend.wohlig.com",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com", "sohan@wohlig.com"]
 
