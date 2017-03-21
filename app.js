dc0d70d85ede37c22902ab2037d63d310e25b35c
sohan1 <shn619@gmail.com>
2017-03-21 15:30:18 +0530

    app.js

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ mongoose = require('mongoose');
 
 global["database"] = "betaTravelibro";
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://104.199.132.38:27017/' + database, function (err) {
-// mongoose.connect('mongodb://localhost:27017/' + database, function (err) {
+// mongoose.connect('mongodb://104.199.132.38:27017/' + database, function (err) {
+mongoose.connect('mongodb://localhost:27017/' + database, function (err) {
     if (err) {
         console.log(err);
     }
